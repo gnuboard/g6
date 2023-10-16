533938a1c061d122e2d0c1d29f840148b780401c
kjh <kjhied@gmail.com>
2023-10-16 12:20:10 +0900

    [feat] Q&A 기능 > 사용자 페이지 추가
    - 기본적인 CRUD 기능 구현
    - 부수적인 기능 (검색/페이징/답변/첨부파일) 추가 예정
    - 연관관계 선언 삭제

--- _bbs/qa.py
+++ _bbs/qa.py
@@ -1,13 +1,13 @@
+from pickletools import string1
 from common import *
 from database import get_db
 from dataclasses import dataclass
-from fastapi import APIRouter, Depends, File, Form, Request, HTTPException
+from fastapi import APIRouter, Depends, File, Form, HTTPException, Query, Request
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 import models
-
 
 router = APIRouter()
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
@@ -20,7 +20,6 @@ class QaContentDataclass:
     """
     1:1문의 폼 데이터
     """
-    qa_name: str = Form(None)
     qa_email: str = Form(None)
     qa_hp: str = Form(None)
     qa_type: int = 0
@@ -41,6 +40,7 @@ def qa_list(request: Request,
     Q&A 목록 보기
     '''
     sca = request.state.sca if request.state.sca is not None else ""
+    stx = request.state.stx if request.state.stx is not None else ""
 
     # Q&A 설정 조회
     qa_config = db.query(models.QaConfig).order_by(models.QaConfig.id.asc()).first()
@@ -50,8 +50,10 @@ def qa_list(request: Request,
     # Q&A 목록 조회
     queryset = db.query(models.QaContent).filter(models.QaContent.qa_type == 0).order_by(models.QaContent.qa_id.desc())
     # 제목과 내용 중 검색어가 있으면 검색한다.
-    if request.state.stx:
-        queryset = queryset.filter(models.Qa.subject.like(f"%{request.state.stx}%") | models.Qa.content.like(f"%{request.state.stx}%"))
+    if sca:
+        queryset = queryset.filter(models.QaContent.qa_category == sca)
+    if stx:
+        queryset = queryset.filter(models.QaContent.qa_subject.like(f"%{stx}%") | models.QaContent.qa_content.like(f"%{stx}%"))
     qa_list = queryset.all()
 
     context = {
@@ -113,11 +115,11 @@ def qa_edit(qa_id: int,
     return templates.TemplateResponse(f"qa/pc/qa_form.html", context)
 
 
-@router.post("/update/{qa_id:int = None}")
+@router.post("/update")
 def qa_update(request: Request,
-                qa_id: int = None,
-                db: Session = Depends(get_db),
                 token: str = Form(...),
+                qa_id: str = Form(None),
+                db: Session = Depends(get_db),
                 form_data: QaContentDataclass = Depends()
                 ):
     """1:1문의 설정 등록/수정 처리
@@ -132,7 +134,14 @@ def qa_update(request: Request,
     Returns:
         RedirectResponse: 1:1문의 설정 등록/수정 후 폼으로 이동
     """
+    # 회원정보
+
     if validate_one_time_token(token, 'create'): # 토큰에 등록돤 action이 create라면 신규 등록
+        form_data.mb_id = ''
+        form_data.qa_name = ''
+        form_data.qa_datetime = datetime.now()
+        form_data.qa_ip = ''
+
         qa = models.QaContent(**form_data.__dict__)
         db.add(qa)
         db.commit()
@@ -147,7 +156,25 @@ def qa_update(request: Request,
     else: # 토큰 검사 실패
         raise HTTPException(status_code=404, detail=f"{token} : 토큰이 존재하지 않습니다.")
 
-    return RedirectResponse(url=f"/qa/view", status_code=302)
+    return RedirectResponse(url=f"/qa/{qa.qa_id}", status_code=302)
+
+
+@router.get("/delete/{qa_id}")
+def qa_delete(qa_id: int,
+              token: str = Query(...),
+              db: Session = Depends(get_db)):
+    '''
+    Q&A 삭제하기
+    '''
+    if not validate_one_time_token(token, 'delete'):
+        raise HTTPException(status_code=404, detail=f"{token} : 토큰이 존재하지 않습니다.")
+
+
+    # Q&A 삭제
+    db.query(models.QaContent).filter(models.QaContent.qa_id == qa_id).delete()
+    db.commit()
+
+    return RedirectResponse(url=f"/qa/list", status_code=302)
 
 
 @router.get("/{qa_id}")
@@ -157,18 +184,24 @@ def qa_view(qa_id: int,
     '''
     Q&A 상세보기
     '''
+    model = models.QaContent
+
     # Q&A 설정 조회
     qa_config = db.query(models.QaConfig).order_by(models.QaConfig.id.asc()).first()
     if not qa_config:
         raise HTTPException(status_code=404, detail=f"Q&A Config is not found.")
     
     # Q&A 조회
-    qa = db.query(models.QaContent).filter(models.QaContent.qa_id == qa_id).first()
+    qa = db.query(model).filter(model.qa_id == qa_id).first()
+
+    # Q&A 답변글 조회
+    answer = db.query(model).filter(model.qa_type == 1, model.qa_parent == qa_id).first()
 
     context = {
         "request": request,
         "outlogin": request.state.context["outlogin"],
         "qa": qa,
+        "answer": answer,
     }
 
     return templates.TemplateResponse(f"qa/pc/qa_view.html", context)
--- models.py
+++ models.py
@@ -232,9 +232,6 @@ class Member(Base):
     mb_9 = Column(String(255), nullable=False, default='')
     mb_10 = Column(String(255), nullable=False, default='')
 
-    # 연관관계
-    qas = relationship("QaContent", back_populates="member", cascade="all, delete-orphan")
-
 
 class Board(Base):
     '''
@@ -603,6 +600,3 @@ class QaContent(Base):
 
     # Index 추가
     qa_num_parent_index = Index('qa_num_parent', qa_num, qa_parent)
-
-    # 연관관계
-    member = relationship("Member", back_populates="qas", foreign_keys=[mb_id])
