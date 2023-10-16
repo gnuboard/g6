5557cebf126ad646921b5d406e70dec2272b2be1
kagla <kagla@naver.com>
2023-10-16 16:12:29 +0900

    접속자검색 수정중

--- _admin/admin_visit.py
+++ _admin/admin_visit.py
@@ -27,6 +27,73 @@ templates.env.globals["get_paging"] = get_paging
 
 VISIT_MENU_KEY = "200800"
 
+@router.get("/visit_search")
+def visit_search(request: Request, db: Session = Depends(get_db),
+        sst: str = Query(default=""), # sort field (정렬 필드)
+        sod: str = Query(default=""), # search order (검색 오름, 내림차순)
+        sfl: str = Query(default=""), # search field (검색 필드)
+        stx: str = Query(default=""), # search text (검색어)
+        current_page: int = Query(default=1, alias="page"), # 페이지
+        ):
+    '''
+    접속자 검색
+    '''
+    request.session["menu_key"] = "200810"
+    
+    # 초기 쿼리 설정
+    query = db.query(models.Visit)
+    records_per_page = request.state.config.cf_page_rows
+    query_string = generate_query_string(request)
+
+    # sod가 제공되면, 해당 열을 기준으로 정렬을 추가합니다.
+    if sst is not None and sst != "":
+        if sod == "desc":
+            query = query.order_by(desc(getattr(models.Visit, sst)))
+        else:
+            query = query.order_by(asc(getattr(models.Visit, sst)))
+
+    # sfl과 stx가 제공되면, 해당 열과 값으로 추가 필터링을 합니다.
+    if sfl is not None and stx is not None:
+        if hasattr(models.Visit, sfl):  # sfl이 models.Board에 존재하는지 확인
+            if sfl in ["vi_ip", "vi_date"]:
+                query = query.filter(getattr(models.Visit, sfl).like(f"{stx}%"))
+            else:
+                query = query.filter(getattr(models.Visit, sfl).like(f"%{stx}%"))
+            
+    # 페이지 번호에 따른 offset 계산
+    offset = (current_page - 1) * records_per_page
+
+    # 전체 레코드 개수 계산
+    total_records = query.count()
+    # 최종 쿼리 결과를 가져옵니다.
+    result = query.offset(offset).limit(records_per_page).all()
+    
+    visits = []
+    for i, row in enumerate(result):
+        referer = row.vi_referer[:255] if row.vi_referer else ""
+        title = referer.replace("<", "&lt;").replace(">", "&gt;")
+        link = f'<a href="{row.vi_referer}" target="_blank" title="{title}">'
+        visits.append({
+            "browser": row.vi_browser,
+            "os": row.vi_os,
+            "device": row.vi_device,
+            "referer": referer,
+            "title": title,
+            "link": link,
+            "ip": row.vi_ip,
+            "date": row.vi_date,
+            "time": row.vi_time,
+        })
+    
+    context = {
+        "request": request,
+        "visits": visits,
+        "total_records": total_records,
+        "paging": get_paging(request, current_page, total_records, f"/admin/visit_search?{query_string}&page="),
+    }
+    return templates.TemplateResponse("visit_search.html", context)
+    
+
 @router.get("/visit_list")
 def visit_list(request: Request, db: Session = Depends(get_db),
         current_page: int = Query(default=1, alias="page"), # 페이지
