e20b63cac90dbd634d61800327114c91d9c3a808
kit rio <mario64aq@gmail.com>
2023-10-23 11:43:54 +0900

    [remove] dev, prod env 구분 삭제

--- config.py
+++ config.py
@@ -1,16 +1,8 @@
-import os
-
 from dotenv import load_dotenv
-from settings import APP_IS_DEBUG
-
 
 # import 순환참조로 인해 common.py 에 둘 수없음.
 def load_gnuboard_env():
     """
     그누보드 6의 설정로딩
     """
-
-    if APP_IS_DEBUG:
-        load_dotenv("dev.env", verbose=True)
-    else:
-        load_dotenv("prod.env", verbose=True)
+    load_dotenv(".env", verbose=True)
