5900009083a218103117103e9b3616f1b47b8cda
kit rio <mario64aq@gmail.com>
2023-11-24 06:42:35 +0900

    [add] 사용자, 관리자 템플릿 클래스 추가
    main 에 register_theme_statics 추가

--- lib/common.py
+++ lib/common.py
@@ -35,6 +35,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from lib.captcha.recaptch_v2 import ReCaptchaV2
 from lib.captcha.recaptch_inv import ReCaptchaInvisible
+from lib.plugin.service import get_admin_plugin_menus
 
 load_dotenv()
 
@@ -1566,6 +1567,96 @@ class StringEncrypt:
 # print(decrypted_text)
 
 
+class UserTemplates(Jinja2Templates):
+    """
+    Jinja2Template 설정 클래스
+    """
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(UserTemplates, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self,
+                 directory: Union[str, os.PathLike],
+                 context_processors: dict = None,
+                 globals: dict = None,
+                 env: Environment = None,
+                 ):
+        super().__init__(directory=directory, context_processors=context_processors)
+        # 공통 env.global 설정
+        self.env.globals["editor_path"] = editor_path
+        self.env.globals["generate_token"] = generate_token
+        self.env.globals["getattr"] = getattr
+        self.env.globals["get_selected"] = get_selected
+        self.env.globals["get_member_icon"] = get_member_icon
+        self.env.globals["get_member_image"] = get_member_image
+        self.env.filters["number_format"] = number_format
+        self.env.globals["theme_asset"] = theme_asset
+
+        # 사용자 템플릿, 관리자 템플릿에 따라 기본 컨텍스트와 env.global 변수를 다르게 설정
+        if TEMPLATES_DIR in directory:
+            self.context_processors.append(self._default_context)
+
+        # 추가 env.global 설정
+        if globals:
+            self.env.globals.update(**globals.__dict__)
+
+    def _default_context(self, request: Request):
+        context = {
+            "menus": get_menus(),
+            "poll": get_recent_poll(),
+            "populars": get_populars(),
+            "latest": latest,
+            "visit": visit,
+        }
+        return context
+
+
+class AdminTemplates(Jinja2Templates):
+    """
+    Jinja2Template 설정 클래스
+    """
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(AdminTemplates, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self,
+                 directory: Union[str, os.PathLike],
+                 context_processors: dict = None,
+                 globals: dict = None,
+                 env: Environment = None
+                 ):
+        super().__init__(directory=directory, context_processors=context_processors)
+        # 공통 env.global 설정
+        self.env.globals["editor_path"] = editor_path
+        self.env.globals["generate_token"] = generate_token
+        self.env.globals["getattr"] = getattr
+        self.env.globals["get_selected"] = get_selected
+        self.env.globals["get_member_icon"] = get_member_icon
+        self.env.globals["get_member_image"] = get_member_image
+        self.env.filters["number_format"] = number_format
+        self.env.globals["theme_asset"] = theme_asset
+
+        # 관리자 템플릿에 따라 기본 컨텍스트와 env.global 변수를 다르게 설정
+        self.context_processors.append(self._default_admin_context)
+
+        # 추가 env.global 설정
+        if globals:
+            self.env.globals.update(**globals.__dict__)
+
+    def _default_admin_context(self, request: Request):
+        context = {
+            "admin_menus": get_admin_menus(),
+            "admin_plugin_menus": get_admin_plugin_menus(),
+        }
+        return context
+
 class MyTemplates(Jinja2Templates):
     """
     Jinja2Template 설정 클래스
@@ -2014,6 +2105,8 @@ def theme_asset(asset_path: str):
 
     theme_path = get_theme_from_db()
     theme_name = theme_path.replace(TEMPLATES + '/', "")
+    print('theme_asset')
+    print(theme_name)
     return f"/theme_static/{theme_name}/{asset_path}"
 
 
