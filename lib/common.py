8adec628cd37f1a5e2e33e692512da48c8ad45e2
kjh <kjhied@gmail.com>
2023-11-28 14:28:29 +0900

    [fix] 관리자페이지 > 관리자권한 수정사항 반영
    - include된 하나의 router파일에서 여러개의 메뉴가 포함될 경우 오류 수정
    - => router함수 내부에서 메뉴를 구분할 수 있도록 tags를 추가
    - admin_menu_bbs.json > 속성 변경(permission 삭제, tag 추가)
    - get_current_admin_menu_id 함수 > 메뉴조회 함수 적용

--- lib/common.py
+++ lib/common.py
@@ -2005,13 +2005,10 @@ def get_current_admin_menu_id(request: Request) -> Optional[str]:
         Optional[str]: 관리자 메뉴 아이디
     """
     try:
+        admin_menu = get_admin_menus()
+
         path = request.url.path
         routes = request.app.routes
-
-        # JSON 파일에서 데이터 로드
-        auth_menu = {}
-        with open('admin/admin_menu_bbs.json', 'r', encoding='utf-8') as file:
-            auth_menu = json.load(file)
 
         for route in routes:
             # 현재 경로가 router 형식에 맞다면
@@ -2019,7 +2016,7 @@ def get_current_admin_menu_id(request: Request) -> Optional[str]:
                 tags = route.tags
                 # 라우터의 태그와 일치하는 메뉴 아이디를 반환
                 for tag in tags:
-                    for menu_items in auth_menu.values():
+                    for menu_items in admin_menu.values():
                         item = next((item for item in menu_items if item.get("tag", "") == tag), None)
                         if item:
                             return item.get("id")
