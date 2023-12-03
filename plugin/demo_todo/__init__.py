d3a96ccaaef9f3ca541cf274e3c21c0e7dd36441
kit rio <mario64aq@gmail.com>
2023-12-03 17:34:23 +0900

    [add] 플러그인 todo 예제

---
+++ plugin/demo_todo/__init__.py
@@ -0,0 +1,12 @@
+module_name = "demo_todo"
+
+__version__ = "0.0.1"
+__plugin_name__ = "데모 todo 플러그인"
+
+# 패키지 외부에서 접근 가능한 모듈 목록
+# "static" 폴더는 제외한다.
+__all__ = [
+    "admin",
+    "router",
+    "module_name"
+]
---
+++ plugin/demo_todo/router/show_router.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+
+from lib.common import TEMPLATES_DIR
+from ..__init__ import module_name
+
+show_router = APIRouter()
+
+PLUGIN_TEMPLATES_DIR = f"plugin/{module_name}/templates"
+templates = Jinja2Templates(directory=[TEMPLATES_DIR, PLUGIN_TEMPLATES_DIR])
+
+
+@show_router.get("/show")
+def show(request: Request):
+    return {"message": "Hello Plugin!"}
+
+
+@show_router.get("/show_template")
+def show(request: Request):
+    return templates.TemplateResponse(
+        "user_demo.html",
+        {
+            "request": request,
+            "title": "Hello user demo plugin!",
+            "content": f"Hello demo plugin!",
+        })
