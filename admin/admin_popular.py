8a1120fa14808f469811bb057e60088bb90af230
kjh <kjhied@gmail.com>
2023-12-13 20:23:46 +0900

    [refact] SQLAlchemy 표현식 개선 > 관리자페이지 > 인기검색어
    - SQLAlchemy 2.0 버전의 select/update/delete 표현식으로 개선
    - 기타 코드 정리 (import 순서 정리, 중복코드/공백 제거, html 수정)

--- admin/admin_popular.py
+++ admin/admin_popular.py
@@ -1,30 +1,27 @@
 from fastapi import APIRouter, Depends, Form, Request
 from fastapi.responses import RedirectResponse
-from sqlalchemy import text
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import aliased
 
-from lib.common import *
 from common.database import db_session
-from lib.plugin.service import get_admin_plugin_menus, get_all_plugin_module_names
 from common.models import Popular
+from lib.common import *
 
 router = APIRouter()
 templates = AdminTemplates()
-templates.env.globals['get_selected'] = get_selected
-templates.env.globals['get_admin_menus'] = get_admin_menus
-templates.env.globals["get_admin_plugin_menus"] = get_admin_plugin_menus
-templates.env.globals["get_all_plugin_module_names"] = get_all_plugin_module_names
 
 LIST_MENU_KEY = "300300"
 RANK_MENU_KEY = "300400"
 
 
 @router.get("/popular_list", tags=["admin_popular_list"])
-async def popular_list(request: Request, db: db_session,
-                 search_params: dict = Depends(common_search_query_params)):
-    '''
+async def popular_list(
+    request: Request,
+    db: db_session,
+    search_params: dict = Depends(common_search_query_params)
+):
+    """
     인기검색어 목록
-    '''
+    """
     request.session["menu_key"] = LIST_MENU_KEY
 
     # 인기검색어 목록 데이터 출력
@@ -47,57 +44,50 @@ async def popular_list(request: Request, db: db_session,
 
 
 @router.post("/popular/delete", dependencies=[Depends(validate_token)], tags=["admin_popular_list"])
-async def popular_delete(request: Request,
-                    db: db_session,
-                    checks: List[int] = Form(..., alias="chk[]")):
-    '''
+async def popular_delete(
+    request: Request,
+    db: db_session,
+    checks: List[int] = Form(..., alias="chk[]")
+):
+    """
     인기검색어 목록 삭제
-    '''
+    """
     # in 조건을 사용해서 일괄 삭제
-    db.query(Popular).filter(Popular.pp_id.in_(checks)).delete()
+    db.execute(delete(Popular).where(Popular.pp_id.in_(checks)))
     db.commit()
 
     # 기존 캐시 삭제
     popular_cache.update({"populars": None})
-        
-    query_string = generate_query_string(request)
 
-    return RedirectResponse(f"/admin/popular_list?{query_string}", status_code=303)
-
+    return RedirectResponse(f"/admin/popular_list?{request.query_params}", status_code=303)
 
 
 @router.get("/popular_rank", tags=["admin_popular_rank"])
-async def popular_rank(request: Request,
-                db: db_session,
-                fr_date: str = Query(default=str(datetime.now().date())),
-                to_date: str = Query(default=str(datetime.now().date())),
-                current_page: int = Query(default=1, alias="page")
-                ):
-    '''
+async def popular_rank(
+    request: Request,
+    db: db_session,
+    fr_date: str = Query(default=str(datetime.now().date())),
+    to_date: str = Query(default=str(datetime.now().date())),
+    current_page: int = Query(default=1, alias="page")
+):
+    """
     인기검색어 순위
-    '''
+    """
     request.session["menu_key"] = RANK_MENU_KEY
+    config = request.state.config
 
+    records_per_page = getattr(config, "cf_page_rows", 10)
     fr_date = re.sub(r'[^0-9 :\-]', '', fr_date)
     to_date = re.sub(r'[^0-9 :\-]', '', to_date)
 
     # 인기검색어 순위 데이터 출력
-    # query = db.query(
-    #         Popular.pp_word,
-    #         func.count(Popular.pp_word).label('search_count'),
-    #         func.rank().over(order_by=desc(text('search_count'))).label('ranking')
-    #     ).filter(
-    #     Popular.pp_word != '',
-    #     Popular.pp_date >= fr_date,
-    #     Popular.pp_date <= to_date
-    # ).group_by(Popular.pp_word).order_by(desc('search_count'), Popular.pp_word)
-    
-    subquery = (
-        db.query(
+    # 인라인 뷰를 사용해서 인기검색어 순위를 구함
+    inline_view = aliased(
+        select(
             Popular.pp_word,
             func.count(Popular.pp_word).label('search_count')
         )
-        .filter(
+        .where(
             Popular.pp_word != '',
             Popular.pp_date >= fr_date,
             Popular.pp_date <= to_date
@@ -105,20 +95,20 @@ async def popular_rank(request: Request,
         .group_by(Popular.pp_word)
         .subquery()
     )
-
-    query = (
-        db.query(
-            subquery.c.pp_word,
-            subquery.c.search_count,
-            func.rank().over(order_by=desc(subquery.c.search_count)).label('ranking')
-        )
-        .order_by(desc(subquery.c.search_count), subquery.c.pp_word)
+    query = select().order_by(
+        desc(inline_view.columns.search_count),
+        inline_view.columns.pp_word
     )
 
-    total_count = query.count()
-    records_per_page = request.state.config.cf_page_rows
+    # 페이징 처리
+    total_count = db.scalar(query.add_columns(func.count(inline_view.columns.pp_word)))
     offset = (current_page - 1) * records_per_page
-    ranks = query.offset(offset).limit(records_per_page).all()
+    ranks = db.execute(
+        query.add_columns(
+            inline_view,
+            func.dense_rank().over(order_by=desc(inline_view.columns.search_count)).label('ranking')  # 순위
+        ).offset(offset).limit(records_per_page)
+    ).all()
 
     context = {
         "request": request,
