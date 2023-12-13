7b419e28aa1b84c0e5ff41359d6087f2e0d37f52
kit rio <mario64aq@gmail.com>
2023-12-13 12:45:02 +0900

    [move] 플러그인 예시 router 파일 이동
    user/user_router.py

--- plugin/demo_plugin/user/router/__init__.py
+++
@@ -1,7 +0,0 @@
-from main import app
-from ...plugin_info import module_name, router_prefix
-from .user_router import router
-
-
-def register_user_router():
-    app.include_router(router, prefix=f"/{router_prefix}", tags=[module_name])
