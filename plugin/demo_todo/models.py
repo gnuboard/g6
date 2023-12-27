af02adcfe19c70a5b69c745955d31733175b3957
kit rio <mario64aq@gmail.com>
2023-12-27 09:56:47 +0900

    [refact] todo 플러그인 DB 커넥션 수정
    engine, DB_TABLE_PREFIX 를 새로 생긴 DBConnect에서 가져오게 변경

--- plugin/demo_todo/models.py
+++ plugin/demo_todo/models.py
@@ -2,12 +2,12 @@ from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
 
-from common.database import DB_TABLE_PREFIX
+from common.database import DBConnect
 from common.models import Base
 
 
 class Todo(Base):
-    __tablename__ = DB_TABLE_PREFIX + "todos"
+    __tablename__ = DBConnect().table_prefix + "todos"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(length=50), index=True)
     content = Column(Text(length=255))
