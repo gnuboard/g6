f0001e65d22234c6359e6fcc0b726c5119290f63
kagla <kagla@naver.com>
2024-01-08 16:29:48 +0900

    todo 플러그인 추가

---
+++ plugin/demo_todo/user/user_router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+
+from core.template import TEMPLATES_DIR, theme_asset
+from ..plugin_config import module_name
+
+router = APIRouter()
+
+PLUGIN_TEMPLATES_DIR = f"plugin/{module_name}/templates"
+templates = Jinja2Templates(directory=[TEMPLATES_DIR, PLUGIN_TEMPLATES_DIR])
+templates.env.globals["theme_asset"] = theme_asset
+
+
+@router.get("/show")
+def show(request: Request):
+    return {"message": "Hello Plugin!"}
+
+
+@router.get("/show_template")
+def show(request: Request):
+    return templates.TemplateResponse(
+        "user_demo.html",
+        {
+            "request": request,
+            "title": f"Hello plugin!",
+            "content": f"Hello {module_name}!",
+        })
