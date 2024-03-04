6b8eb684d48e1387045741d5c9299122d8e8834d
kjh <kjhied@gmail.com>
2024-03-04 09:55:28 +0900

    [fix] 비회원 글 삭제시 토큰 오류 수정
    - Query String에 token 파라미터가 중복으로 추가되지 않도록 처리

--- lib/board_lib.py
+++ lib/board_lib.py
@@ -1188,7 +1188,7 @@ def delete_write(request: Request, bo_table: str, origin_write: WriteBaseModel) 
             raise AlertException("자신의 게시글만 삭제할 수 있습니다.", 403)
         elif not origin_write.mb_id and not request.session.get(f"ss_delete_{bo_table}_{origin_write.wr_id}"):
             url = f"/bbs/password/delete/{bo_table}/{origin_write.wr_id}"
-            query_params = request.query_params
+            query_params = remove_query_params(request, "token")
             raise AlertException("비회원 글을 삭제할 권한이 없습니다.", 403, set_url_query_params(url, query_params))
     
     # 답변글이 있을 때 삭제 불가
