ee7aebca41f302baa9307b64ec8a51974da13599
Junanjunan <taltalrealty@gmail.com>
2024-03-05 16:19:13 +0900

    [refact] JWT 생성 메소드 > token type에 따라 분기

--- api/v1/auth/jwt.py
+++ api/v1/auth/jwt.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Union
+from enum import Enum
 
 from fastapi import HTTPException, status
 from jose import ExpiredSignatureError, JWTError, jwt
@@ -8,65 +8,39 @@ from api.settings import SETTINGS
 from api.v1.models.auth import TokenPayload
 
 
+class TokenType(Enum):
+    ACCESS = {
+        "secret_key": SETTINGS.ACCESS_TOKEN_SECRET_KEY,
+        "expires_minute": SETTINGS.ACCESS_TOKEN_EXPIRE_MINUTES
+    }
+    REFRESH = {
+        "secret_key": SETTINGS.REFRESH_TOKEN_SECRET_KEY,
+        "expires_minute": SETTINGS.REFRESH_TOKEN_EXPIRE_MINUTES
+    }
+
 class JWT:
     """JWT 관련 작업을 처리하는 클래스입니다."""
 
     JWT_TYPE = "Bearer"
 
-    def _encode_jwt(
-            self,
-            data: dict,
-            secret_key: str,
-            expires_minute: Union[float, None]) -> str:
+    @staticmethod
+    def create_token(data: dict, token_type: TokenType) -> str:
         """JWT를 생성합니다.
 
         Args:
             data (dict): JWT에 담을 데이터
-            secret_key (str): JWT 암호화 키
-            expires_minute (Union[float, None]): 만료 시간 (분)
-
+            token_type (TokenType): JWT 종류 (ACCESS, REFRESH)
         Returns:
             str: JWT
         """
-
+        secret_key = token_type.value["secret_key"]
+        expires_minute = token_type.value["expires_minute"]
         to_encode = data.copy()
         expire = datetime.now() + timedelta(minutes=expires_minute)
         to_encode.update({"exp": expire})
+        print(f"Settings.{token_type.name.lower()}_token_secret_key", secret_key)
 
         return jwt.encode(to_encode, secret_key, algorithm=SETTINGS.AUTH_ALGORITHM)
-
-    @staticmethod
-    def create_access_token(data: dict) -> str:
-        """Access Token을 생성합니다.
-
-        Args:
-            data (dict): Access Token에 담을 데이터
-
-        Returns:
-            str: Access Token
-        """
-        print("SETTINGS.ACCESS_TOKEN_SECRET_KEY", SETTINGS.ACCESS_TOKEN_SECRET_KEY)
-        jwt = JWT()
-        return jwt._encode_jwt(
-            data,
-            SETTINGS.ACCESS_TOKEN_SECRET_KEY,
-            SETTINGS.ACCESS_TOKEN_EXPIRE_MINUTES)
-
-    @staticmethod
-    def create_refresh_token(data: dict) -> str:
-        """Refresh Token을 생성합니다.
-
-        Args:
-            data (dict): Refresh Token에 담을 데이터
-
-        Returns:
-            str: Refresh Token
-        """
-        jwt = JWT()
-        return jwt._encode_jwt(
-            data,
-            SETTINGS.REFRESH_TOKEN_SECRET_KEY,
-            SETTINGS.REFRESH_TOKEN_EXPIRE_MINUTES)
 
     @staticmethod
     def decode_token(token: str, secret_key: str) -> dict:
