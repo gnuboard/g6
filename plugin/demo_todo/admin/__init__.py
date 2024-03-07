be4b565051d10762ce567119debad21e53c91062
kit rio <mario64aq@gmail.com>
2024-03-07 17:55:00 +0900

    [add] 플러그인 라우터 관리자체크 의존성 주가
    관리자는 라우터에 Depends(check_admin_access) 의존성을 추가 해야 접근권한이 보호된다.

--- plugin/demo_todo/admin/__init__.py
+++ plugin/demo_todo/admin/__init__.py
@@ -1,3 +1,6 @@
+from fastapi.params import Depends
+
+from lib.dependencies import check_admin_access
 from main import app
 from .. import plugin_config
 from ..admin.admin_router import admin_router
@@ -5,9 +8,9 @@ from ..plugin_config import module_name
 
 
 # 플러그인의 admin 라우터를 등록한다.
-# 관리자는 /admin 으로 시작해야 접근권한이 보호된다.
+# 관리자는 Depends(check_admin_access) 의존성을 추가 해야 접근권한이 보호된다.
 def register_admin_router():
-    app.include_router(admin_router, prefix="/admin", tags=[module_name])
+    app.include_router(admin_router, prefix="/admin", tags=[module_name], dependencies=[Depends(check_admin_access)])
 
 
 def register_admin_menu():
