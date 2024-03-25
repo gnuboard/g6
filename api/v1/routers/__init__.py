db2e5d82169e25cb22e279f311a12fa5840c848f
kjh <kjhied@gmail.com>
2024-03-25 17:21:05 +0900

    [style] REST API > Tags class 코드 스타일 변경

--- api/v1/routers/__init__.py
+++ api/v1/routers/__init__.py
@@ -1,27 +1,31 @@
+"""API 1 Version Router Module."""
 from enum import Enum
 
 from fastapi import APIRouter, Depends
 
 from lib.dependencies import check_use_api
-from api.v1.routers import auth, member, memo, point, scrap, board, menu
+from api.v1.routers import (
+    auth, board, member, memo, menu, point, scrap
+)
 
 
 class Tags(Enum):
-    auth = "인증"
-    members = "회원"
-    memos = "쪽지"
-    menu = "메뉴"
-    points = "포인트"
-    scraps = "스크랩"
-    board = "게시판"
+    """API 태그를 정의합니다."""
+    AUTH = "인증"
+    MEMBER = "회원"
+    MEMO = "쪽지"
+    MENU = "메뉴"
+    POINT = "포인트"
+    SCRAP = "스크랩"
+    BOARD = "게시판"
 
 
 # API 버전 1 라우터를 정의합니다.
 router = APIRouter(prefix="/api/v1", dependencies=[Depends(check_use_api)])
-router.include_router(auth.router, prefix="", tags=[Tags.auth])
-router.include_router(member.router, prefix="", tags=[Tags.members])
-router.include_router(memo.router, prefix="/member", tags=[Tags.memos])
-router.include_router(point.router, prefix="/member", tags=[Tags.points])
-router.include_router(scrap.router, prefix="/member", tags=[Tags.scraps])
-router.include_router(board.router, prefix="/board", tags=[Tags.board])
-router.include_router(menu.router, prefix="", tags=[Tags.menu])
+router.include_router(auth.router, prefix="", tags=[Tags.AUTH])
+router.include_router(member.router, prefix="", tags=[Tags.MEMBER])
+router.include_router(memo.router, prefix="/member", tags=[Tags.MEMO])
+router.include_router(point.router, prefix="/member", tags=[Tags.POINT])
+router.include_router(scrap.router, prefix="/member", tags=[Tags.SCRAP])
+router.include_router(board.router, prefix="/board", tags=[Tags.BOARD])
+router.include_router(menu.router, prefix="", tags=[Tags.MENU])
