be50d045f507c74cf12063ebfd821d545ca0908e
Junanjunan <taltalrealty@gmail.com>
2024-03-29 18:10:53 +0900

    [refact] Template/API 코드 통합 > 불필요한 변수 및 체인 메소드 제거

--- response_handlers/board/delete_post.py
+++ response_handlers/board/delete_post.py
@@ -71,56 +71,54 @@ class DeletePostService(BoardService):
 
     def delete_write(self):
         """게시글 삭제 처리"""
-        origin_write = self.write
         write_model = self.write_model
         db = self.db
         bo_table = self.bo_table
         board = self.board
-        request = self.request
 
         # 원글 + 댓글
         delete_write_count = 0
         delete_comment_count = 0
         writes = db.scalars(
             select(write_model)
-            .filter_by(wr_parent=origin_write.wr_id)
+            .filter_by(wr_parent=self.wr_id)
             .order_by(write_model.wr_id)
         ).all()
         for write in writes:
             # 원글 삭제
             if not write.wr_is_comment:
                 # 원글 포인트 삭제
-                if not delete_point(request, write.mb_id, board.bo_table, write.wr_id, "쓰기"):
-                    insert_point(request, write.mb_id, board.bo_write_point * (-1), f"{board.bo_subject} {write.wr_id} 글 삭제")
+                if not delete_point(self.request, write.mb_id, bo_table, self.wr_id, "쓰기"):
+                    insert_point(self.request, write.mb_id, board.bo_write_point * (-1), f"{board.bo_subject} {self.wr_id} 글 삭제")
                 # 파일+섬네일 삭제
-                BoardFileManager(board, write.wr_id).delete_board_files()
+                BoardFileManager(board, self.wr_id).delete_board_files()
 
                 delete_write_count += 1
                 # TODO: 에디터 섬네일 삭제
             else:
                 # 댓글 포인트 삭제
-                if not delete_point(request, write.mb_id, board.bo_table, write.wr_id, "댓글"):
-                    insert_point(request, write.mb_id, board.bo_comment_point * (-1), f"{board.bo_subject} {write.wr_id} 댓글 삭제")
+                if not delete_point(self.request, write.mb_id, bo_table, self.wr_id, "댓글"):
+                    insert_point(self.request, write.mb_id, board.bo_comment_point * (-1), f"{board.bo_subject} {self.wr_id} 댓글 삭제")
 
                 delete_comment_count += 1
 
         # 원글+댓글 삭제
-        db.execute(delete(write_model).filter_by(wr_parent=origin_write.wr_id))
+        db.execute(delete(write_model).filter_by(wr_parent=self.wr_id))
 
         # 최근 게시물 삭제
         db.execute(delete(BoardNew).where(
             BoardNew.bo_table == bo_table,
-            BoardNew.wr_parent == origin_write.wr_id
+            BoardNew.wr_parent == self.wr_id
         ))
 
         # 스크랩 삭제
         db.execute(delete(Scrap).filter_by(
             bo_table=bo_table,
-            wr_id=origin_write.wr_id
+            wr_id=self.wr_id
         ))
 
         # 공지사항 삭제
-        board.bo_notice = self.set_board_notice(origin_write.wr_id, False)
+        board.bo_notice = self.set_board_notice(self.wr_id, False)
 
         # 게시글 갯수 업데이트
         board.bo_count_write -= delete_write_count
