5adf55679bac6db56d3cfe4c725b71190fd0c63b
kjh <kjhied@gmail.com>
2024-05-13 17:19:48 +0900

    [fix] 포인트 내역조회 오류 수정
    - get_login_member() 의존성 함수 > 세션관련 오류로 인한 오류 수정

--- lib/dependency/auth.py
+++ lib/dependency/auth.py
@@ -1,13 +1,20 @@
-from fastapi import Request
+from typing import Union
+from typing_extensions import Annotated
+from fastapi import Depends, Request
 
 from core.exception import AlertException
 from core.models import Member
+from service.member_service import MemberService
 
 
-def get_login_member(request: Request):
+def get_login_member(
+    request: Request,
+    service: Annotated[MemberService, Depends()]
+) -> Member:
     """현재 로그인 여부 검사 진행 후 로그인 멤버를 반환한다."""
-    member: Member = request.state.login_member
-    if not member:
+    mb_id = request.session["ss_mb_id"]
+    member: Member = service.fetch_member_by_id(mb_id)
+    if not member or not mb_id:
         path = request.url.path
         url = request.url_for("login_form").replace_query_params(url=path)
         raise AlertException("로그인 후 이용 가능합니다.", 403, url=url)
@@ -15,7 +22,11 @@ def get_login_member(request: Request):
     return member
 
 
-def get_login_member_optional(request: Request) -> Member:
+def get_login_member_optional(
+    request: Request,
+    service: Annotated[MemberService, Depends()]
+) -> Union[Member, None]:
     """현재 로그인 멤버를 반환한다. 로그인이 되어 있지 않으면 None을 반환한다."""
-    member: Member = request.state.login_member
+    mb_id = request.session["ss_mb_id"]
+    member: Member = service.fetch_member_by_id(mb_id)
     return member
