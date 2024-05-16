08c6a9e0fd2bcbe3af2d45bc5340812c6f70cc02
kjh <kjhied@gmail.com>
2024-05-16 18:25:01 +0900

    [fix] 플러그인 > 사용자페이지 접근시 오류 수정
    - 사용자페이지에 필요한 의존성 주입 추가

--- plugin/demo_plugin/user/__init__.py
+++ plugin/demo_plugin/user/__init__.py
@@ -1,7 +1,14 @@
+from fastapi import Depends
+from lib.dependency.dependencies import check_use_template, set_template_basic_data
 from main import app
+from .user_router import router
 from ..plugin_config import module_name, router_prefix
-from ..user.user_router import router
 
 
 def register_user_router():
-    app.include_router(router, prefix=router_prefix, tags=[module_name], include_in_schema=False)
+    app.include_router(router,
+                       prefix=router_prefix,
+                       tags=[module_name],
+                       include_in_schema=False,
+                       dependencies=[Depends(check_use_template),
+                                     Depends(set_template_basic_data)])
