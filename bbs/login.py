06643208b5939e8ef50556d6acdd68129d7003b0
Junanjunan <taltalrealty@gmail.com>
2024-05-17 10:22:08 +0900

    [fix] 동기함수에 잘못 사용된 await 제거 (starlette)

--- bbs/login.py
+++ bbs/login.py
@@ -51,9 +51,9 @@ async def login(
     if auto_login and not is_super_admin(request):
         age_1day = 60 * 60 * 24
         cookie_domain = request.state.cookie_domain
-        await response.set_cookie(key="ck_mb_id", value=member.mb_id,
+        response.set_cookie(key="ck_mb_id", value=member.mb_id,
                             max_age=age_1day * 30, domain=cookie_domain)
-        await response.set_cookie(key="ck_auto", value=ss_mb_key,
+        response.set_cookie(key="ck_auto", value=ss_mb_key,
                             max_age=age_1day * 30, domain=cookie_domain)
 
     return response
@@ -90,8 +90,8 @@ async def logout(request: Request):
 
     response = RedirectResponse(url="/", status_code=302)
     if "ck_auto" in request.cookies:
-        await response.delete_cookie(key="ck_auto")
+        response.delete_cookie(key="ck_auto")
     if "ck_mb_id" in request.cookies:
-        await response.delete_cookie(key="ck_mb_id")
+        response.delete_cookie(key="ck_mb_id")
 
     return response
